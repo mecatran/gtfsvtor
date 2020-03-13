8b479b459c2cfa89a9ffdaaff16c49367d316764
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-03-13 11:01:35 +0100

    Add stop with parent and timezone validation.

--- src/main/java/com/mecatran/gtfsvtor/validation/streaming/StopStreamingValidator.java
+++ src/main/java/com/mecatran/gtfsvtor/validation/streaming/StopStreamingValidator.java
@@ -4,6 +4,8 @@ import static com.mecatran.gtfsvtor.validation.impl.StreamingValidationUtils.che
 
 import com.mecatran.gtfsvtor.model.GtfsStop;
 import com.mecatran.gtfsvtor.model.GtfsStopType;
+import com.mecatran.gtfsvtor.reporting.ReportSink;
+import com.mecatran.gtfsvtor.reporting.issues.InvalidFieldValueError;
 import com.mecatran.gtfsvtor.validation.StreamingValidateType;
 import com.mecatran.gtfsvtor.validation.StreamingValidator;
 
@@ -13,6 +15,7 @@ public class StopStreamingValidator implements StreamingValidator<GtfsStop> {
 	@Override
 	public void validate(Class<? extends GtfsStop> clazz, GtfsStop stop,
 			StreamingValidator.Context context) {
+		ReportSink reportSink = context.getReportSink();
 		GtfsStopType stopType = stop.getType();
 		boolean namePosMandatory = stopType == GtfsStopType.STOP
 				|| stopType == GtfsStopType.STATION
@@ -20,6 +23,12 @@ public class StopStreamingValidator implements StreamingValidator<GtfsStop> {
 		if (namePosMandatory) {
 			checkNonNull(stop::getLat, "stop_lat", context);
 			checkNonNull(stop::getLon, "stop_lon", context);
+		}
+		if (stop.getParentId() != null && stop.getTimezone() != null) {
+			reportSink.report(new InvalidFieldValueError(
+					context.getSourceInfo(), stop.getTimezone().getID(),
+					"stop having a parent should not define a timezone",
+					"stop_timezone"));
 		}
 		// Note: Parent station validity is checked in the reference validator
 	}
