61db74d897b46f82fa895ce2c57f748e58fe5209
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-04-10 14:30:56 +0200

    Add method to get today as logical date.

--- src/main/java/com/mecatran/gtfsvtor/model/GtfsLogicalDate.java
+++ src/main/java/com/mecatran/gtfsvtor/model/GtfsLogicalDate.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class GtfsLogicalDate implements Comparable<GtfsLogicalDate> {
 
@@ -86,6 +87,12 @@ public class GtfsLogicalDate implements Comparable<GtfsLogicalDate> {
 		// TODO synchronize CACHE?
 		return CACHE.computeIfAbsent(julianDay,
 				jd -> new GtfsLogicalDate(year, month, day, jd));
+	}
+
+	public static GtfsLogicalDate today(TimeZone timezone) {
+		Calendar cal = GregorianCalendar.getInstance(timezone);
+		return getDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1,
+				cal.get(Calendar.DAY_OF_MONTH));
 	}
 
 	public GtfsLogicalDate offset(int nDays) {
