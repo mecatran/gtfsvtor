d977cf17e4072f329170db3da841aabb49008bdc
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-04-14 16:32:12 +0200

    Add Pair utility class.

---
+++ src/main/java/com/mecatran/gtfsvtor/utils/Pair.java
@@ -0,0 +1,42 @@
+package com.mecatran.gtfsvtor.utils;
+
+import java.util.Objects;
+
+public class Pair<E1, E2> {
+	// implements Comparable<ComparablePair<E1, E2>> {
+
+	private E1 a;
+	private E2 b;
+
+	public Pair(E1 a, E2 b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public E1 getFirst() {
+		return a;
+	}
+
+	public E2 getSecond() {
+		return b;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(a, b);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		Pair<?, ?> other = (Pair<?, ?>) obj;
+		return Objects.equals(a, other.a) && Objects.equals(b, other.b);
+	}
+
+	public String toString() {
+		return "{" + a + ", " + b + "}";
+	}
+}
