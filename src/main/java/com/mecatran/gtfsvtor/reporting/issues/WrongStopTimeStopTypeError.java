be2dee526692188fe2437972f0d3a4a2af273acb
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-04-15 14:36:48 +0200

    Validate time stop type in streaming validator.

--- src/main/java/com/mecatran/gtfsvtor/reporting/issues/WrongStopTimeStopTypeError.java
+++ src/main/java/com/mecatran/gtfsvtor/reporting/issues/WrongStopTimeStopTypeError.java
@@ -1,36 +1,29 @@
 package com.mecatran.gtfsvtor.reporting.issues;
 
-import com.mecatran.gtfsvtor.model.GtfsRoute;
+import java.util.Arrays;
+import java.util.List;
+
+import com.mecatran.gtfsvtor.loader.DataObjectSourceInfo;
 import com.mecatran.gtfsvtor.model.GtfsStop;
 import com.mecatran.gtfsvtor.model.GtfsStopTime;
-import com.mecatran.gtfsvtor.model.GtfsTrip;
 import com.mecatran.gtfsvtor.reporting.IssueFormatter;
 import com.mecatran.gtfsvtor.reporting.ReportIssue;
 import com.mecatran.gtfsvtor.reporting.ReportIssuePolicy;
 import com.mecatran.gtfsvtor.reporting.ReportIssueSeverity;
+import com.mecatran.gtfsvtor.reporting.SourceInfoWithFields;
 
 @ReportIssuePolicy(severity = ReportIssueSeverity.ERROR, categoryName = "Wrong time stop type")
 public class WrongStopTimeStopTypeError implements ReportIssue {
 
-	private GtfsRoute route;
-	private GtfsTrip trip;
 	private GtfsStopTime stopTime;
 	private GtfsStop stop;
+	private SourceInfoWithFields sourceInfo;
 
-	public WrongStopTimeStopTypeError(GtfsRoute route, GtfsTrip trip,
+	public WrongStopTimeStopTypeError(DataObjectSourceInfo sourceInfo,
 			GtfsStopTime stopTime, GtfsStop stop) {
-		this.route = route;
-		this.trip = trip;
 		this.stopTime = stopTime;
 		this.stop = stop;
-	}
-
-	public GtfsRoute getRoute() {
-		return route;
-	}
-
-	public GtfsTrip getTrip() {
-		return trip;
+		this.sourceInfo = new SourceInfoWithFields(sourceInfo, "stop_id");
 	}
 
 	public GtfsStopTime getStopTime() {
@@ -42,13 +35,14 @@ public class WrongStopTimeStopTypeError implements ReportIssue {
 	}
 
 	@Override
+	public List<SourceInfoWithFields> getSourceInfos() {
+		return Arrays.asList(sourceInfo);
+	}
+
+	@Override
 	public void format(IssueFormatter fmt) {
-		fmt.text(
-				"Wrong type {0} for stop {1} at sequence {2} @ {3} in trip {4} of route {5} {6}, should be type STOP (0)",
+		fmt.text("Wrong type {0} for stop {1} {2}, should be type STOP (0)",
 				fmt.pre(stop.getType().toString()), fmt.id(stop.getId()),
-				fmt.pre(stopTime.getStopSequence().toString()),
-				fmt.time(stopTime.getDepartureOrArrivalTime()),
-				fmt.id(trip.getId()), fmt.id(route.getId()),
-				fmt.var(route.getShortName()));
+				fmt.var(stop.getName()));
 	}
 }
--- src/main/java/com/mecatran/gtfsvtor/validation/streaming/StopTimeStreamingValidator.java
+++ src/main/java/com/mecatran/gtfsvtor/validation/streaming/StopTimeStreamingValidator.java
@@ -7,6 +7,7 @@ import com.mecatran.gtfsvtor.model.GtfsDropoffType;
 import com.mecatran.gtfsvtor.model.GtfsPickupType;
 import com.mecatran.gtfsvtor.model.GtfsStop;
 import com.mecatran.gtfsvtor.model.GtfsStopTime;
+import com.mecatran.gtfsvtor.model.GtfsStopType;
 import com.mecatran.gtfsvtor.model.GtfsTimepoint;
 import com.mecatran.gtfsvtor.model.GtfsTrip;
 import com.mecatran.gtfsvtor.reporting.ReportSink;
@@ -15,6 +16,7 @@ import com.mecatran.gtfsvtor.reporting.issues.InvalidReferenceError;
 import com.mecatran.gtfsvtor.reporting.issues.MissingMandatoryValueError;
 import com.mecatran.gtfsvtor.reporting.issues.TimeTravelAtStopError;
 import com.mecatran.gtfsvtor.reporting.issues.UselessTimepointWarning;
+import com.mecatran.gtfsvtor.reporting.issues.WrongStopTimeStopTypeError;
 import com.mecatran.gtfsvtor.validation.StreamingValidateType;
 import com.mecatran.gtfsvtor.validation.StreamingValidator;
 
@@ -36,18 +38,30 @@ public class StopTimeStreamingValidator
 
 		ReadOnlyDao dao = context.getPartialDao();
 		// stoptime->trip reference
-		if (stopTime.getTripId() != null
-				&& dao.getTrip(stopTime.getTripId()) == null) {
-			reportSink.report(new InvalidReferenceError(context.getSourceInfo(),
-					"trip_id", stopTime.getTripId().getInternalId(),
-					GtfsTrip.TABLE_NAME, "trip_id"));
+		if (stopTime.getTripId() != null) {
+			GtfsTrip trip = dao.getTrip(stopTime.getTripId());
+			if (trip == null) {
+				reportSink.report(
+						new InvalidReferenceError(context.getSourceInfo(),
+								"trip_id", stopTime.getTripId().getInternalId(),
+								GtfsTrip.TABLE_NAME, "trip_id"));
+			}
 		}
-		// stoptime->stop reference
-		if (stopTime.getStopId() != null
-				&& dao.getStop(stopTime.getStopId()) == null) {
-			reportSink.report(new InvalidReferenceError(context.getSourceInfo(),
-					"stop_id", stopTime.getStopId().getInternalId(),
-					GtfsStop.TABLE_NAME, "stop_id"));
+		if (stopTime.getStopId() != null) {
+			GtfsStop stop = dao.getStop(stopTime.getStopId());
+			// stoptime->stop reference
+			if (stop == null) {
+				reportSink.report(
+						new InvalidReferenceError(context.getSourceInfo(),
+								"stop_id", stopTime.getStopId().getInternalId(),
+								GtfsStop.TABLE_NAME, "stop_id"));
+			} else {
+				// stop type
+				if (stop.getType() != GtfsStopType.STOP) {
+					reportSink.report(new WrongStopTimeStopTypeError(
+							context.getSourceInfo(), stopTime, stop));
+				}
+			}
 		}
 		// Departure/arrival should be either set or not
 		if (stopTime.getDepartureTime() == null
