8f9c8488363a7745dc093c75fc991894a9a70f70
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-04-20 15:41:04 +0200

    Add feed-info and pathways to statistics.

--- src/main/java/com/mecatran/gtfsvtor/validation/dao/StatisticsValidator.java
+++ src/main/java/com/mecatran/gtfsvtor/validation/dao/StatisticsValidator.java
@@ -17,11 +17,12 @@ public class StatisticsValidator implements DaoValidator {
 		ReportSink reportSink = context.getReportSink();
 
 		reportSink.report(new StatisticsInfo(String.format(
-				"Loaded %d agencies, %d routes, %d stops, %d calendars, %d calendar dates, %d trips, %d stop times, %d transfers, %d fare attributes, %d fare rules",
-				dao.getAgencies().size(), dao.getRoutes().size(),
-				dao.getStops().size(), dao.getCalendars().size(),
-				dao.getCalendarDates().size(), dao.getTrips().size(),
-				dao.getStopTimesCount(), dao.getTransfers().size(),
+				"Loaded %d feed-info, %d agencies, %d routes, %d stops, %d calendars, %d calendar dates, %d trips, %d stop times, %d transfers, %d pathways, %d fare attributes, %d fare rules",
+				dao.getFeedInfo() == null ? 0 : 1, dao.getAgencies().size(),
+				dao.getRoutes().size(), dao.getStops().size(),
+				dao.getCalendars().size(), dao.getCalendarDates().size(),
+				dao.getTrips().size(), dao.getStopTimesCount(),
+				dao.getTransfers().size(), dao.getPathways().size(),
 				dao.getFareAttributes().size(), dao.getFareRulesCount())));
 	}
 }
