78390ed6befff96bb844672c4d81f8cf34f92fb8
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-04-30 14:07:25 +0200

    Print source info in text printout.

--- src/main/java/com/mecatran/gtfsvtor/reporting/impl/PlainTextIssueFormatter.java
+++ src/main/java/com/mecatran/gtfsvtor/reporting/impl/PlainTextIssueFormatter.java
@@ -2,7 +2,6 @@ package com.mecatran.gtfsvtor.reporting.impl;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.mecatran.gtfsvtor.loader.DataObjectSourceInfo;
@@ -49,23 +48,76 @@ public class PlainTextIssueFormatter implements IssueFormatter {
 	}
 
 	public static String format(ReportIssue issue) {
-		// TODO Add source context infos
 		PlainTextIssueFormatter fmt = new PlainTextIssueFormatter();
 		issue.format(fmt);
 		StringBuffer sb = new StringBuffer();
+		sb.append("\n");
 		for (SourceInfoWithFields siwf : issue.getSourceInfos()) {
 			DataObjectSourceInfo dosi = siwf.getSourceInfo();
 			TableSourceInfo tsi = dosi.getTable();
-			sb.append(tsi.getTableName());
-			sb.append(", line ").append(dosi.getLineNumber());
-			sb.append(": ");
-			List<String> fieldNames = new ArrayList<>(siwf.getFieldNames());
-			Collections.sort(fieldNames);
-			sb.append(String.join(", ", fieldNames));
-			sb.append("\n");
+			int nCols = tsi.getHeaderColumns().size();
+			int nVals = dosi.getFields() == null ? 0 : dosi.getFields().size();
+			int tbLen = tsi.getTableName().length();
+			List<Integer> colWidths = new ArrayList<>();
+			for (int i = 0; i < nCols; i++) {
+				int headerWidth = tsi.getHeaderColumns().get(i).length();
+				int valueWidth = 0;
+				if (i < nVals) {
+					String val = dosi.getFields().get(i);
+					valueWidth = val == null ? 0 : val.length();
+				}
+				colWidths.add(Math.max(headerWidth, valueWidth));
+			}
+			sb.append(pad(dosi.getTable().getTableName(), tbLen + 1, ' '));
+			for (int i = 0; i < nCols; i++) {
+				sb.append("| ").append(pad(tsi.getHeaderColumns().get(i),
+						colWidths.get(i), ' ')).append(" ");
+			}
+			sb.append("|\n");
+			sb.append(pad("", tbLen + 1, ' '));
+			for (int i = 0; i < nCols; i++) {
+				boolean highlight = siwf.getFieldNames()
+						.contains(tsi.getHeaderColumns().get(i));
+				sb.append(highlight ? "+=" : "+-")
+						.append(pad("", colWidths.get(i),
+								highlight ? '=' : '-'))
+						.append(highlight ? "=" : "-");
+			}
+			sb.append("+\n");
+			if (nVals > 0) {
+				sb.append(pad(String.format("L%d", dosi.getLineNumber()),
+						tbLen + 1, ' '));
+				for (int i = 0; i < nCols && i < nVals; i++) {
+					String val = dosi.getFields().get(i);
+					sb.append("| ").append(
+							pad(val == null ? "" : val, colWidths.get(i), ' '))
+							.append(" ");
+				}
+				sb.append("|\n");
+				sb.append(pad("", tbLen + 1, ' '));
+				for (int i = 0; i < nCols; i++) {
+					boolean highlight = siwf.getFieldNames()
+							.contains(tsi.getHeaderColumns().get(i));
+					sb.append(highlight ? "+=" : "+-")
+							.append(pad("", colWidths.get(i),
+									highlight ? '=' : '-'))
+							.append(highlight ? "=" : "-");
+				}
+				sb.append("+\n");
+			}
 		}
 		sb.append(issue.getSeverity()).append(": ");
 		sb.append(fmt.getPlainTextResult());
+		return sb.toString();
+
+	}
+
+	private static String pad(String s, int n, char pad) {
+		StringBuilder sb = new StringBuilder(n);
+		sb.append(s);
+		while (sb.length() < n) {
+			sb.append(pad);
+		}
 		return sb.toString();
 	}
 }
