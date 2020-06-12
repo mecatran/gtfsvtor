802ed1173831a3231403322e0160400fad92697a
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-06-12 11:39:45 +0200

    Make use of Histogram helper in stats validator.

--- src/main/java/com/mecatran/gtfsvtor/validation/triptimes/StopTimesStatsValidator.java
+++ src/main/java/com/mecatran/gtfsvtor/validation/triptimes/StopTimesStatsValidator.java
@@ -1,14 +1,11 @@
 package com.mecatran.gtfsvtor.validation.triptimes;
 
-import java.io.PrintStream;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import com.mecatran.gtfsvtor.model.GtfsLogicalTime;
 import com.mecatran.gtfsvtor.model.GtfsStopTime;
 import com.mecatran.gtfsvtor.model.GtfsTripAndTimes;
+import com.mecatran.gtfsvtor.utils.Histogram;
 import com.mecatran.gtfsvtor.validation.DaoValidator.Context;
 import com.mecatran.gtfsvtor.validation.DefaultDisabledValidator;
 import com.mecatran.gtfsvtor.validation.TripTimesValidator;
@@ -16,28 +13,12 @@ import com.mecatran.gtfsvtor.validation.TripTimesValidator;
 @DefaultDisabledValidator
 public class StopTimesStatsValidator implements TripTimesValidator {
 
-	private static class DeltaStats {
-		private Map<Integer, AtomicInteger> deltaCount = new HashMap<>();
-
-		private void stat(int delta) {
-			deltaCount.computeIfAbsent(delta, d -> new AtomicInteger())
-					.addAndGet(1);
-		}
-
-		private void print(PrintStream ps) {
-			deltaCount.entrySet().stream().sorted(
-					(e1, e2) -> Integer.compare(e1.getKey(), e2.getKey()))
-					.forEach(e -> ps.println(String.format("%5ds - %d",
-							e.getKey(), e.getValue().get())));
-		}
-	}
-
-	private DeltaStats ds1, ds2;
+	private Histogram<Integer> hop, slack;
 
 	@Override
 	public void start(Context context) {
-		ds1 = new DeltaStats();
-		ds2 = new DeltaStats();
+		hop = new Histogram<Integer>("Hop times");
+		slack = new Histogram<Integer>("Slack times");
 	}
 
 	@Override
@@ -51,16 +32,16 @@ public class StopTimesStatsValidator implements TripTimesValidator {
 			GtfsLogicalTime arrivalTime = stopTime.getArrivalOrDepartureTime();
 			if (arrivalTime != null) {
 				if (lastValidStopTime != null) {
-					int deltaSec = arrivalTime.getSecondSinceMidnight()
+					int hopSec = arrivalTime.getSecondSinceMidnight()
 							- lastValidStopTime.getDepartureOrArrivalTime()
 									.getSecondSinceMidnight();
-					ds1.stat(deltaSec);
+					hop.count(hopSec);
 				}
 				lastValidStopTime = stopTime;
 				if (departureTime != null) {
-					int deltaSec2 = departureTime.getSecondSinceMidnight()
+					int slackSec = departureTime.getSecondSinceMidnight()
 							- arrivalTime.getSecondSinceMidnight();
-					ds2.stat(deltaSec2);
+					slack.count(slackSec);
 				}
 			}
 		}
@@ -69,8 +50,8 @@ public class StopTimesStatsValidator implements TripTimesValidator {
 	@Override
 	public void end(Context context) {
 		System.out.println("Departure -> Arrival sec delays histogram:");
-		ds1.print(System.out);
+		System.out.println(hop);
 		System.out.println("Arrival -> Departure sec delays histogram:");
-		ds2.print(System.out);
+		System.out.println(slack);
 	}
 }
