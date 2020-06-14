535e68c9070224c2721040cb0985bb0a32b75d45
Holger Bruch <hb@mfdz.de>
2020-06-14 07:04:08 +0200

    Adds new standard route types 11 and 12 (and not yet adopted 9)

--- src/main/java/com/mecatran/gtfsvtor/model/GtfsRouteType.java
+++ src/main/java/com/mecatran/gtfsvtor/model/GtfsRouteType.java
@@ -18,6 +18,12 @@ public class GtfsRouteType {
 	public static final int CABLE_CAR_CODE = 5;
 	public static final int GONDOLA_CODE = 6;
 	public static final int FUNICULAR_CODE = 7;
+	// Note: Intercity bus code is not yet officially adopted
+	// (see https://github.com/google/transit/pull/174)
+	// As these may have higher maxspeed values, we support them nevertheless
+	public static final int INTERCITY_BUS_CODE = 9;
+	public static final int TROLLEYBUS_CODE = 11;
+	public static final int MONORAIL_CODE = 12;
 
 	public static final GtfsRouteType TRAM = new GtfsRouteType(TRAM_CODE);
 	public static final GtfsRouteType METRO = new GtfsRouteType(METRO_CODE);
@@ -29,6 +35,12 @@ public class GtfsRouteType {
 	public static final GtfsRouteType GONDOLA = new GtfsRouteType(GONDOLA_CODE);
 	public static final GtfsRouteType FUNICULAR = new GtfsRouteType(
 			FUNICULAR_CODE);
+	public static final GtfsRouteType INTERCITY_BUS = new GtfsRouteType(
+			INTERCITY_BUS_CODE);
+	public static final GtfsRouteType TROLLEYBUS = new GtfsRouteType(
+			TROLLEYBUS_CODE);
+	public static final GtfsRouteType MONORAIL = new GtfsRouteType(
+			MONORAIL_CODE);
 
 	private static Map<Integer, GtfsRouteType> CACHE = new HashMap<>(10);
 
