e69905fad9df0a4149c9739dcefcc91c009935fb
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-06-23 08:27:05 +0200

    Add alias for commonly used GtfsIdIndexer.

--- src/main/java/com/mecatran/gtfsvtor/dao/impl/GtfsIdIndexer.java
+++ src/main/java/com/mecatran/gtfsvtor/dao/impl/GtfsIdIndexer.java
@@ -4,16 +4,22 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.mecatran.gtfsvtor.model.GtfsId;
 import com.mecatran.gtfsvtor.model.GtfsObject;
+import com.mecatran.gtfsvtor.model.GtfsStop;
 
 public class GtfsIdIndexer<U, V extends GtfsObject<U>, T extends GtfsId<U, V>> {
 
 	private BiMap<T, Integer> indexes = HashBiMap.create();
 
 	// Keep 0 for special values if needed
+	// TODO Insert 0 for null
 	private int nextIndex = 1;
 
 	public GtfsIdIndexer() {
 	}
+
+	public static class GtfsStopIdIndexer
+			extends GtfsIdIndexer<String, GtfsStop, GtfsStop.Id> {
+	};
 
 	/**
 	 * Insert the value if not present, return the associated index
