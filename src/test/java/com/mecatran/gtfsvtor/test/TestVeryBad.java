93682767f43f98258013c916bcd9c3119a9d9f67
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2020-06-26 12:47:11 +0200

    Add unit test on the "verybad" dataset.

---
+++ src/test/java/com/mecatran/gtfsvtor/test/TestVeryBad.java
@@ -0,0 +1,38 @@
+package com.mecatran.gtfsvtor.test;
+
+import static com.mecatran.gtfsvtor.test.TestUtils.loadAndValidate;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.mecatran.gtfsvtor.loader.DataObjectSourceInfo;
+import com.mecatran.gtfsvtor.model.GtfsAgency;
+import com.mecatran.gtfsvtor.reporting.ReportIssueSeverity;
+import com.mecatran.gtfsvtor.reporting.issues.DuplicatedColumnError;
+import com.mecatran.gtfsvtor.test.TestUtils.TestBundle;
+
+public class TestVeryBad {
+
+	@Test
+	public void testVerybad() {
+		TestBundle tb = loadAndValidate("verybad");
+
+		// This *will* break, check and adjust when necessary
+		assertEquals(6, tb.issuesCountOfSeverities(ReportIssueSeverity.INFO));
+		assertEquals(104,
+				tb.issuesCountOfSeverities(ReportIssueSeverity.WARNING));
+		assertEquals(121,
+				tb.issuesCountOfSeverities(ReportIssueSeverity.ERROR));
+		assertEquals(0,
+				tb.issuesCountOfSeverities(ReportIssueSeverity.CRITICAL));
+
+		// Check if source info are properly loaded
+		DuplicatedColumnError dce = tb.report
+				.getReportIssues(DuplicatedColumnError.class).findFirst().get();
+		assertEquals(1, dce.getSourceRefs().size());
+		DataObjectSourceInfo si = tb.report
+				.getSourceInfo(dce.getSourceRefs().get(0).getSourceRef());
+		assertEquals(GtfsAgency.TABLE_NAME, si.getTable().getTableName());
+		assertEquals(1, si.getLineNumber());
+	}
+}
