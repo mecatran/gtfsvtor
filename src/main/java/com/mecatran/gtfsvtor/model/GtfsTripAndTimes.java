fca5a99262dbcdfc399183b347ce918b4219d41d
Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
2022-08-03 20:53:37 +0200

    Add helper method to get first/last time of trip.

--- src/main/java/com/mecatran/gtfsvtor/model/GtfsTripAndTimes.java
+++ src/main/java/com/mecatran/gtfsvtor/model/GtfsTripAndTimes.java
@@ -1,9 +1,10 @@
 package com.mecatran.gtfsvtor.model;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
- * A simple holder for a trip, it's associated list of stop times, and a pattern
+ * A simple holder for a trip, its associated list of stop times, and a pattern
  * key.
  */
 public interface GtfsTripAndTimes {
@@ -11,6 +12,56 @@ public interface GtfsTripAndTimes {
 	public GtfsTrip getTrip();
 
 	public List<GtfsStopTime> getStopTimes();
+
+	/**
+	 * @return The first stop time in the list, if present.
+	 */
+	public default Optional<GtfsStopTime> getFirstStopTime() {
+		List<GtfsStopTime> stopTimes = getStopTimes();
+		if (stopTimes == null || stopTimes.isEmpty())
+			return Optional.empty();
+		else
+			return Optional.of(stopTimes.get(0));
+	}
+
+	/**
+	 * @return The first stop departure time, if present and defined. Fall back
+	 *         to arrival time if defined but departure is not.
+	 */
+	public default Optional<GtfsLogicalTime> getFirstStopTimeDeparture() {
+		Optional<GtfsStopTime> stopTime = getFirstStopTime();
+		if (stopTime.isPresent()) {
+			return Optional
+					.ofNullable(stopTime.get().getDepartureOrArrivalTime());
+		} else {
+			return Optional.empty();
+		}
+	}
+
+	/**
+	 * @return The last stop time in the list, if present.
+	 */
+	public default Optional<GtfsStopTime> getLastStopTime() {
+		List<GtfsStopTime> stopTimes = getStopTimes();
+		if (stopTimes == null || stopTimes.isEmpty())
+			return Optional.empty();
+		else
+			return Optional.of(stopTimes.get(stopTimes.size() - 1));
+	}
+
+	/**
+	 * @return The last stop arrival time, if presend and defined. Fall back to
+	 *         departure time if defined but arrival is not.
+	 */
+	public default Optional<GtfsLogicalTime> getLastStopTimeArrival() {
+		Optional<GtfsStopTime> stopTime = getLastStopTime();
+		if (stopTime.isPresent()) {
+			return Optional
+					.ofNullable(stopTime.get().getArrivalOrDepartureTime());
+		} else {
+			return Optional.empty();
+		}
+	}
 
 	/**
 	 * @return An opaque object that is guaranteed to be distinct (in regard to
